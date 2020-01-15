f221f62ff19bb2a12a9af6175817947729f80317
swopnil <swopnil420@gmail.com>
2020-01-15 07:47:41 +0600

    defined directory separator and path

---
+++ resources/config.php
@@ -0,0 +1,9 @@
+<?php
+
+// Define directory separator
+define('DS', DIRECTORY_SEPARATOR);
+
+// Define directory path
+define('TEMPLATE_FRONT', __DIR__ . DS . 'templates/front');
+define('TEMPLATE_BACK', __DIR__ . DS . 'templates/back');
+
