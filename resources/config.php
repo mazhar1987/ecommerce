2a666b5f2266fdd6a85408fae9858ff5d4f0f85b
swopnil <swopnil420@gmail.com>
2020-01-15 07:55:13 +0600

    DB connect with constants

--- resources/config.php
+++ resources/config.php
@@ -1,5 +1,8 @@
 <?php
 
+ob_start();
+session_start();
+
 // Define directory separator
 define('DS', DIRECTORY_SEPARATOR);
 
@@ -7,3 +10,14 @@ define('DS', DIRECTORY_SEPARATOR);
 define('TEMPLATE_FRONT', __DIR__ . DS . 'templates/front');
 define('TEMPLATE_BACK', __DIR__ . DS . 'templates/back');
 
+// DB connect with constant
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '91221');
+define('DB_NAME', 'ecommerce');
+
+// DB connect query
+$connect = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+// Add functions file
+require_once 'functions.php';
