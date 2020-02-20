25f8ba564c4c1d17bd36f8bbb461beb4abd54b41
swopnil <swopnil420@gmail.com>
2020-02-20 07:25:39 +0600

    Adding paypal payment system

--- public/checkout.php
+++ public/checkout.php
@@ -17,7 +17,9 @@ include_once(TEMPLATE_FRONT . DS . 'top_nav.php');
 
       <h1>Checkout</h1>
 
-<form action="">
+<form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post">
+    <input type="hidden" name="cmd" value="_cart">
+    <input type="hidden" name="business" value="business@mazhar.com">
     <table class="table table-striped">
         <thead>
           <tr>
@@ -32,6 +34,7 @@ include_once(TEMPLATE_FRONT . DS . 'top_nav.php');
             <?php shoppingCart(); ?>
         </tbody>
     </table>
+    <input type="image" name="submit" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynow_LG.gif" alt="PayPal - The safer, easier way to pay online">
 </form>
 
 
--- resources/functions.php
+++ resources/functions.php
@@ -259,6 +259,10 @@ function shoppingCart() {
 
     $total = 0;
     $item_quantity = 0;
+    $item_name = 1;
+    $item_number = 1;
+    $amount = 1;
+    $quantity = 1;
 
     foreach ($_SESSION as $name => $value) {
         if ($value > 0) {
@@ -284,9 +288,17 @@ function shoppingCart() {
                                 <a href="cart.php?delete={$row['product_id']}" class="btn btn-danger"><span class="glyphicon glyphicon-remove"></span></a>
                             </td>
                         </tr>
+                        <input type="hidden" name="item_name_{$item_name}" value="hat">
+                        <input type="hidden" name="item_number_{$item_number}" value="123">
+                        <input type="hidden" name="amount_{$amount}" value="15.00">
+                        <input type="hidden" name="quantity_{$quantity}" value="1">
                     DELIMETER;
 
                     echo $product_cart;
+                    $item_name++;
+                    $item_number++;
+                    $amount++;
+                    $quantity++;
                 }
 
                 $_SESSION['item_total'] = $total += $subTotal;
