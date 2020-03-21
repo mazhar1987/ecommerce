2c4231e1a78b911561b1a9535d2d51f7b6c8ca87
swopnil <swopnil420@gmail.com>
2020-03-21 08:56:38 +0600

    Added file delete function while delete slider item from admin

--- resources/templates/back/delete_slide.php
+++ resources/templates/back/delete_slide.php
@@ -2,6 +2,15 @@
 require_once('../../resources/config.php');
 
 if (isset($_GET['delete_slider_id'])) {
+
+    // Delete image file from directory
+    $find_image_query = query("SELECT slider_image FROM sliders WHERE slider_id=". escape_string($_GET['delete_slider_id']) ." LIMIT 1");
+    confirm($find_image_query);
+    $row = fetch_array($find_image_query);
+    $target_path = UPLOAD_DIRECTORY . DS . $row['slider_image'];
+    unlink($target_path);
+
+    // Delete slider item from system and DB
     $slide_delete_query = query("DELETE FROM sliders WHERE slider_id = ". escape_string($_GET['delete_slider_id']) ." ");
     confirm($slide_delete_query);
 
